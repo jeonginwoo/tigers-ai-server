8c6b80bc8021d5c5d0382a65ebc4e9993be60066
topblame <113543762+topblame@users.noreply.github.com>
2025-11-29 13:13:56 +0900

    문서 업로드 및 분석결과 출력
    구글 인증을 바탕으로로그인 세션유지를 위한 계정 라우터 및  분석 포트 분석 어댑터

--- documents/application/port/document_repository_port.py
+++ documents/application/port/document_repository_port.py
@@ -1,16 +1,29 @@
 from abc import ABC, abstractmethod
-from typing import List
+from typing import List, Optional, Any
+from fastapi import UploadFile
+
 from documents.domain.document import Document
+
 
 class DocumentRepositoryPort(ABC):
     @abstractmethod
-    async def upload(self):
-        pass
-        
+    async def upload(self, file: UploadFile) -> tuple[str | None, str | None]:
+        ...
+
     @abstractmethod
     def save(self, document: Document) -> Document:
-        pass
+        ...
 
     @abstractmethod
     def find_all(self) -> List[Document]:
-        pass
+        ...
+
+    @abstractmethod
+    def find_by_id(self, document_id: int) -> Optional[Document]:
+        ...
+
+    @abstractmethod
+    def update_result(
+        self, document_id: int, result: dict, status: str | None = None
+    ) -> Document:
+        ...
